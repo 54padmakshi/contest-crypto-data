ff1908c022346c4def3e978e1e287abe7fd35937
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2022-12-10 17:14:47 +0530

    updated

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ console.log("working");  */
  callFetchData();
 
 
-
 /*
+
 function fetchData(){
     const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
     fetch(url)
